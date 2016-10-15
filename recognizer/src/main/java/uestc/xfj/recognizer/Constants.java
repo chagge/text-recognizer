857e56a236e4b91c64a60c73c113917dc512857c
shiqifeng <18581871304@163.com>
2016-10-15 16:07:43 +0800

    江山如画入手中

---
+++ recognizer/src/main/java/uestc/xfj/recognizer/Constants.java
@@ -0,0 +1,11 @@
+package uestc.xfj.recognizer;
+
+/**
+ * Created by shiqifeng on 2016/10/15.
+ * Mail:byhieg@gmail.com
+ */
+
+public class Constants {
+    //权限的标识
+    public static final int PERMISSION = 0x001;
+}
--- recognizer/src/main/java/uestc/xfj/recognizer/MainActivity.java
+++ recognizer/src/main/java/uestc/xfj/recognizer/MainActivity.java
@@ -1,13 +1,82 @@
 package uestc.xfj.recognizer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public Toolbar toolbar;
+    public ImageView imageView;
+    private static final String  TAG  = "uestc.xfj.recognizer.MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        imageView = (ImageView) findViewById(R.id.img_show);
+        toolbar.setTitle("文字识别");
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.take_pic:
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                                Log.e(TAG, "还是没有权限啊");
+                                requestPermissions(new String[]{Manifest.permission.CAMERA,
+                                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                        Constants.PERMISSION);
+                            }else{
+                                Toast.makeText(MainActivity.this, "开始相机调用", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(MainActivity.this, "开始相机调用", Toast.LENGTH_SHORT).show();
+                            Log.e(TAG, "已经有权限了");
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions,grantResults);
+        switch(requestCode) {
+            // requestCode即所声明的权限获取码，在checkSelfPermission时传入
+            case Constants.PERMISSION:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e(TAG, "已经获取到权限了");
+                    // 获取到权限，作相应处理（调用定位SDK应当确保相关权限均被授权，否则可能引起定位失败）
+                }
+                else {
+                    // 没有获取到权限，做特殊处理
+                    Log.e(TAG, "获取权限失败");
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
